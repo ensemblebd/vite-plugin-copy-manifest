964553322906b731334a3c34ed613dbc00e54306
Larry Zhao <thehiddendepth@gmail.com>
2023-01-27 15:59:36 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import * as path from 'path';
+import * as fs from 'fs';
+
+export default function copyManifest(options = {}) {
+  return {
+    name: 'vite-plugin-copy-manifest',
+    configResolved(resolvedConfig) {
+      const outDir = resolvedConfig.build.outDir;
+      const manifestFilename = typeof resolvedConfig.build.manifest === 'string' ? resolvedConfig.build.manifest : 'manifest.json';
+
+      resolvedConfig.plugins.forEach(plugin => {
+        if (plugin.name != 'vite-plugin-copy-manifest') {
+          return;
+        }
+        plugin.writeBundle = () => {
+          const src = path.resolve(outDir + manifestFilename);
+          const dest = path.resolve(options.destDir + manifestFilename);
+
+          fs.copyFile(src, dest, (err) => {
+            if (err) throw err;
+          });
+        }
+      });
+    },
+  };
+}
