93fac27a163eaaec0bcb0d45e3b74b7a4a32aea2
ensemblebd <bdurand@ensemblegroup.net>
2024-07-08 17:46:06 -0500

    Vite manifest path is no longer at root outDir.
    It's stored in a ".vite" folder.
    Also added override option in case that changes in future.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import * as path from 'path';
 import * as fs from 'fs';
 
 export default function copyManifest(options = {}) {
+  const defaultManifestPath = ".vite";
   return {
     name: 'vite-plugin-copy-manifest',
     configResolved(resolvedConfig) {
+      let manifestPath = options.manifestPath ? options.manifestPath : defaultManifestPath;
+      
       const outDir = resolvedConfig.build.outDir;
       const manifestFilename = typeof resolvedConfig.build.manifest === 'string' ? resolvedConfig.build.manifest : 'manifest.json';
 
@@ -13,8 +16,8 @@ export default function copyManifest(options = {}) {
           return;
         }
         plugin.writeBundle = () => {
-          const src = path.resolve(outDir + manifestFilename);
-          const dest = path.resolve(options.destDir + manifestFilename);
+          const src = path.resolve(outDir, manifestPath, manifestFilename);
+          const dest = path.resolve(options.destDir, manifestFilename);
 
           fs.copyFileSync(src, dest);
         }
